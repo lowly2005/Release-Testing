0f7479cfe23ce3682328509946c9360383bbf286
lowly2005 <aker.loubna@gmail.com>
2018-06-27 13:33:58 -0400

    feat:test feat

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => (
       </div>
       <ul className="left">
         <li>
-          <Link to="/">Home branch_1</Link>
+          <Link to="/">Home branch_3</Link>
         </li>
         <li>
           <Link to="/about">About branch_1</Link>
