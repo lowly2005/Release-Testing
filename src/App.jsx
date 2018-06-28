a44191f2cd6c85b01a2f01f8d23cb1b9a91618aa
lowly2005 <aker.loubna@gmail.com>
2018-06-28 10:12:24 -0400

    fix(button): button color

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => (
       </div>
       <ul className="left">
         <li>
-          <Link to="/">Home branch test</Link>
+          <Link to="/">Home branch test bug</Link>
         </li>
         <li>
           <Link to="/about">About branch_b</Link>
