55c80ec4fd31e4253565b80c36d7b1bd139d5bd5
lowly2005 <aker.loubna@gmail.com>
2018-06-28 16:13:51 -0400

    BREACKING CHANGE: yolo breacks

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => (
       </div>
       <ul className="left">
         <li>
-          <Link to="/">Home branch test guu(</Link>
+          <Link to="/">Home branch test guusss</Link>
         </li>
         <li>
           <Link to="/about">About branch_b</Link>
